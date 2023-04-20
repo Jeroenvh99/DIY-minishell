e013c9b046a639ef0670d8e70c4c30aba1739bca
Jeroen Van Halderen <jvan-hal@f0r3s3.codam.nl>
2023-04-20 16:54:27 +0200

    made some tests for pwd command and did some setup

---
+++ source/builtins/pwd.c
@@ -0,0 +1,26 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        ::::::::            */
+/*   pwd.c                                              :+:    :+:            */
+/*                                                     +:+                    */
+/*   By: jvan-hal <jvan-hal@student.codam.nl>         +#+                     */
+/*                                                   +#+                      */
+/*   Created: 2023/04/20 16:27:41 by jvan-hal      #+#    #+#                 */
+/*   Updated: 2023/04/20 16:49:45 by jvan-hal      ########   odam.nl         */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "./../../lib/libft/include/ft_string.h"
+#include <stdlib.h>
+#include <unistd.h>
+
+int	main(int argc, char **argv)
+{
+	char *buf;
+
+	buf = NULL;
+	buf = getcwd(buf, 0);
+	write(1, buf, ft_strlen(buf));
+	free(buf);
+	return (0);
+}
