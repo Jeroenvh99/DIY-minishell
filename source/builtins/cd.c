612763e6fd3fcae0a00fb0aec091053e465c6ef5
Jeroen Van Halderen <jvan-hal@f0r4s3.codam.nl>
2023-04-21 17:37:04 +0200

    worked a bit more on cd command

--- source/builtins/cd.c
+++ source/builtins/cd.c
@@ -6,27 +6,76 @@
 /*   By: jvan-hal <jvan-hal@student.codam.nl>         +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2023/04/20 16:52:40 by jvan-hal      #+#    #+#                 */
-/*   Updated: 2023/04/21 16:25:39 by jvan-hal      ########   odam.nl         */
+/*   Updated: 2023/04/21 17:36:31 by jvan-hal      ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "./../../lib/libft/include/ft_string.h"
-#include <unistd.h>
 #include <stdlib.h>
+#include <unistd.h>
+
+void	write_joined(char *string, size_t len1, size_t len2)
+{
+	int	i;
+
+	i = 0;
+	while (i < len1)
+	{
+		string[i] = *s1;
+		++i;
+		++s1;
+	}
+	string[i] = '/';
+	++i;
+	len2 += len1;
+	while (i < len2)
+	{
+		string[i] = *s2;
+		++i;
+		++s2;
+	}
+}
+
+char	*ft_strjoin_dir(char const *s1, char const *s2)
+{
+	char	*string;
+	size_t	len1;
+	size_t	len2;
+
+	len1 = ft_strlen(s1);
+	len2 = ft_strlen(s2);
+	string = malloc((len1 + len2 + 2) * sizeof(char));
+	if (string == NULL)
+		return (NULL);
+	i = 0;
+	while (i < len1)
+	{
+		string[i] = *s1;
+		++i;
+		++s1;
+	}
+	string[i] = '/';
+	++i;
+	len2 += len1 + 1;
+	while (i < len2)
+	{
+		string[i] = *s2;
+		++i;
+		++s2;
+	}
+	return (string);
+}
 
 int	main(int argc, char **argv)
 {
 	char	*buf;
-	char	*folder;
 	char	*newdir;
 
 	buf = NULL;
 	buf = getcwd(buf, 0);
 	if (!buf)
 		return (1);
-	folder = ft_strjoin("/", argv[1]);
-	newdir = ft_strjoin(buf, folder);
-	free(folder);
+	newdir = ft_strjoin_dir(buf, argv[1]); // don't end path with a /
 	free(buf);
 	chdir(newdir);
 	// update env
--- source/builtins/pwd.c
+++ source/builtins/pwd.c
@@ -6,31 +6,23 @@
 /*   By: jvan-hal <jvan-hal@student.codam.nl>         +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2023/04/20 16:27:41 by jvan-hal      #+#    #+#                 */
-/*   Updated: 2023/04/21 11:16:20 by jvan-hal      ########   odam.nl         */
+/*   Updated: 2023/04/21 17:16:38 by jvan-hal      ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "./../../lib/libft/include/ft_string.h"
-#include <dirent.h>
 #include <stdlib.h>
 #include <unistd.h>
-#include <sys/types.h>
 
 int	main(int argc, char **argv)
 {
-	DIR				*cdir;
-	char			*buf;
-	struct dirent	*dir;
+	char	*buf;
 
 	buf = NULL;
 	buf = getcwd(buf, 0);
 	if (buf)
 	{
 		write(1, buf, ft_strlen(buf));
-		cdir = opendir(buf);
-		dir = readdir(cdir);
-		write(1, dir->d_name, ft_strlen(dir->d_name));
-		closedir(cdir);
 		free(buf);
 		return (0);
 	}
