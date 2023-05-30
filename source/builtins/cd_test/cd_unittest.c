9127b4125ca97884401e73ebd5f33a7f1abb9655
Jeroen Van Halderen <jvan-hal@f0r2s9.codam.nl>
2023-05-30 13:05:54 +0200

    started work on the cd test

--- source/builtins/cd_test/cd_unittest.c
+++ source/builtins/cd_test/cd_unittest.c
@@ -18,20 +18,55 @@
 #include <stdlib.h>
 #include <unistd.h>
 
+void redirect_stdout(void)
+{
+    cr_redirect_stdout();
+}
+
+void redirect_stderr(void)
+{
+    cr_redirect_stderr();
+}
+
+void	assert_cd_case(t_cmd *cmd, char *expected)
+{
+    t_msh	msh;
+    char	*environ[] = {"LOGNAME=jvan-hal". "OLDPWD=/tmp/cd-dash", NULL};
+
+    bzero(&msh, sizeof(msh));
+    msh.env.envp = environ;
+    msh_cd(cmd, &msh);
+    fflush(stdout);
+    cr_assert_stdout_eq_str(expected);
+}
+
+void	assert_cd_dir(t_cmd *cmd, char *expected)
+{
+    t_msh	msh;
+    char	*environ[] = {"LOGNAME=jvan-hal". "OLDPWD=/tmp/cd-dash", NULL};
+
+    bzero(&msh, sizeof(msh));
+    msh.env.envp = environ;
+    msh_cd(cmd, &msh);
+    cr_assert_eq(getcwd(buf, 0), expected);
+}
+
+TestSuite(cd, .init=redirect_stdout);
+
+TestSuite(cd_err, .init=redirect_stderr);
+
 Test(cd, no_arg_with_home)
 {
-	t_msh	msh;
 	t_cmd	cmd;
 	char	*buf;
-	char	*environ[] = {"HOME=/tmp/cd-no_arg_with_home", NULL};
 	char	*input[] = {"cd", NULL};
 
 	buf = NULL;
 	cmd.argc = 1;
 	cmd.argv.array = input;
-	msh.env = environ;
 	system("mkdir /tmp/cd-no_arg_with_home");
-	msh_cd(&cmd, &msh);
-	cr_assert_eq(getcwd(buf, 0), getenv("HOME"));
+	system("cd /tmp/cd-no_arg_with_home");
+	msh_cd(&cmd);
+    assert_cd_dir(cmd, getenv("HOME"));
 	system("rmdir /tmp/cd-no_arg_with_home");
 }
