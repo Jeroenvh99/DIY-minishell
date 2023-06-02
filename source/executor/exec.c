cb58c1d9337ee3551909bc1a20575dc1660f9981
Jeroen Van Halderen <jvan-hal@f0r5s6.codam.nl>
2023-06-02 11:47:17 +0200

    try to fix the brew timeout

--- source/executor/exec.c
+++ source/executor/exec.c
@@ -6,17 +6,48 @@
 /*   By: jvan-hal <jvan-hal@student.codam.nl>         +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2023/05/16 15:12:17 by jvan-hal      #+#    #+#                 */
-/*   Updated: 2023/05/23 17:44:56 by jvan-hal      ########   odam.nl         */
+/*   Updated: 2023/06/02 11:29:15 by jvan-hal      ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
+
+#include "ft_string.h"
+#include "msh.h"
+#include <unistd.h>
+
+int	getbuiltin(char *cmd)
+{
+	if (ft_strnstr(cmd, "cd", 3))
+		return (0);
+	if (ft_strnstr(cmd, "echo", 5))
+		return (1);
+	if (ft_strnstr(cmd, "env", 4))
+		return (2);
+	if (ft_strnstr(cmd, "exit", 5))
+		return (3);
+	if (ft_strnstr(cmd, "pwd", 4))
+		return (4);
+	if (ft_strnstr(cmd, "unset", 6))
+		return (5);
+	return (-1);
+}
 
 int	exec(t_msh *msh)
 {
 	int		*status;
 	pid_t	pid;
+	int		bi;
+	const int	(*builtins[6])(t_cmd * cmd, t_msh * msh) = { cd, echo, env, exit, pwd, unset };
 
-	else
+	while (msh->cmds)
 	{
+		bi = getbuiltin(msh->cmds->cmd->argv.array[0]) if (bi > -1)
+		{
+			msh->status = builtins[bi](msh->cmds->cmd, msh);
+		}
+		else
+		{
+			execve(path, msh->cmds->cmd->argv.array, msh->env);
+		}
 	}
 	return (0);
 }
@@ -29,13 +60,12 @@ static void	child_process(int *tube, char **envp, int fd, t_info *state)
 		|| close(tube[0]) == -1)
 		exit(errno);
 	state->comm_paths[state->index] = get_path(state->paths,
-			state->comm_argv[state->index][0]);
+												state->comm_argv[state->index][0]);
 	if (builtin)
 		i = builtin()
-		exit(i);
+			exit(i);
 	else
-		execve(state->comm_paths[state->index], cmd->argv,
-			msh->env);
+		execve(state->comm_paths[state->index], cmd->argv, msh->env);
 	exit(errno);
 }
 
@@ -46,7 +76,7 @@ static void	parent_end(t_info *state, int child_id, int *tube)
 	if (close(tube[0]) == -1)
 	{
 		msh->last_status = ;
-		return -1;
+		return (-1);
 	}
 	waitpid(child_id, &status, 0);
 	if (WIFEXITED(status))
@@ -55,7 +85,7 @@ static void	parent_end(t_info *state, int child_id, int *tube)
 
 int	exec_command(char **envp, int fd, t_info *state)
 {
-	pid_t	pid;
+	pid_t pid;
 
 	pid = fork_if_necessary(tube);
 	if (pid == 0)
