376557d79d140263769f584e778fa4da09ab784d
Jeroen Van Halderen <jvan-hal@f1r2s2.codam.nl>
2023-06-06 16:42:53 +0200

    please just run the test

--- source/builtins/exit_test/exit_unittest.c
+++ source/builtins/exit_test/exit_unittest.c
@@ -34,7 +34,6 @@ void	assert_exit_output(t_cmd *cmd, char *expected)
     cmd->io.out = 1;
 	msh = (t_msh *)malloc(sizeof(t_msh));
 	bzero(msh, sizeof(t_msh));
-	msh->env = (t_env *)malloc(sizeof(t_env));
 	msh_exit(cmd, msh);
 	fflush(stderr);
 	cr_assert_stdout_eq_str(expected);
@@ -47,7 +46,6 @@ void	assert_exit_output_error(t_cmd *cmd, char *expected)
     cmd->io.err = 2;
 	msh = (t_msh *)malloc(sizeof(t_msh));
 	bzero(msh, sizeof(t_msh));
-	msh->env = (t_env *)malloc(sizeof(t_env));
 	msh_exit(cmd, msh);
 	fflush(stderr);
 	cr_assert_stderr_eq_str(expected);
@@ -61,7 +59,6 @@ void	assert_exit_status(t_cmd *cmd, int expected)
     cmd->io.err = 2;
 	msh = (t_msh *)malloc(sizeof(t_msh));
 	bzero(msh, sizeof(t_msh));
-	msh->env = (t_env *)malloc(sizeof(t_env));
 	status = msh_exit(cmd, msh);
 	cr_assert_eq(status, expected);
 }
