0c59f96131a7e2f25576c7416b88c1d3239b1271
D.P. Bastings <dp.bastings@posteo.net>
2023-06-20 21:40:25 +0200

    More executor stuff. It won't have to open pipes anymore.

---
+++ source/execute/execute.c
@@ -0,0 +1,44 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        ::::::::            */
+/*   execute.c                                          :+:    :+:            */
+/*                                                     +:+                    */
+/*   By: jvan-hal <jvan-hal@student.codam.nl>         +#+                     */
+/*       dbasting <dbasting@student.codam.nl>        +#+                      */
+/*   Created: 2023/05/16 15:12:17 by jvan-hal      #+#    #+#                 */
+/*   Updated: 2023/06/20 21:40:05 by dbasting      ########   odam.nl         */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "msh_execute.h"
+#include "msh.h"
+#include "msh_error.h"
+#include "msh_utils.h"
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+
+/* Execute `cmdtree`. `cmdtree` is consumed. */
+t_errno	execute(t_list **pipeline, t_msh *msh)
+{
+	env_pack(&msh->env);
+	return (execute_pipeline(pipeline, msh));
+}
+
+/* Execute all commands in `pipeline` `pipeline` is consumed. */
+t_errno execute_pipeline(t_list **pipeline, t_msh *msh)
+{
+	t_cmd	*cmd;
+	t_errno	errno;
+
+	while (*pipeline)
+	{
+		cmd = list_pop_ptr(pipeline);
+		errno = execute_cmd(cmd, msh);
+		cmd_free(cmd);
+		if (errno != MSH_SUCCESS)
+			return (list_clear(pipeline, (t_freef)cmd_free), errno);
+	}
+	return (MSH_SUCCESS);
+}
--- source/execute/execute_cmd.c
+++ source/execute/execute_cmd.c
@@ -1,44 +1,55 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        ::::::::            */
-/*   exec.c                                             :+:    :+:            */
+/*   execute_cmd.c                                      :+:    :+:            */
 /*                                                     +:+                    */
 /*   By: jvan-hal <jvan-hal@student.codam.nl>         +#+                     */
-/*                                                   +#+                      */
+/*       dbasting <dbasting@student.codam.nl>        +#+                      */
 /*   Created: 2023/05/16 15:12:17 by jvan-hal      #+#    #+#                 */
-/*   Updated: 2023/06/19 18:08:44 by dbasting      ########   odam.nl         */
+/*   Updated: 2023/06/20 14:30:26 by dbasting      ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "ft_string.h"
+#include "msh_execute.h"
 #include "msh.h"
+#include "msh_error.h"
+
+#include "ft_string.h"
 #include <stdlib.h>
 #include <sys/wait.h>
 #include <unistd.h>
 
+static void			launch(t_cmd *cmd, t_msh *msh);
+static t_builtinf	get_builtin(char const *cmd)
+
 t_errno	execute(t_list **pipeline, t_msh *msh)
 
-int	execute_cmd(t_cmd *cmd, t_msh *msh)
+t_errno	execute_cmd(t_cmd *cmd, t_msh *msh)
 {
 	t_builtinf const	builtin = is_builtin(cmd->argv.array[0]);
 	int					wstatus;
-	pid_t				pid;
 
 	if (builtin)
 		return (builtin(cmd->argc, cmd->argv.array, msh->env.envp)); // tenzij builtin in pijplin zit; dan moet er een subshell komen / fork()
-	pid = fork();
-	if (pid == -1)
-		return (perror("msh:"), MSH_FORKFAIL);
-	if (pid == 0)
-		execve(path, msh->cmds->cmd->argv.array, msh->env.envp);
-	waitpid(pid, &wstatus);
+	msh->g_msh->child = fork();
+	if (msh->g_msh->child == -1)
+		return (perror("msh/execute"), MSH_FORKFAIL);
+	if (msh->g_msh->child == 0)
+		launch(cmd, msh);
+	waitpid(msh->g_msh->child, &wstatus);
 	if (WIFEXITED(wstatus))
-		return (WEXITSTATUS(wstatus));
-	if (WIFSIGNALED(wstatus))
-		return (128 + WTERMSIG(wstatus));
+		msh->g_msh->exit = WEXITSTATUS(wstatus);
+	else if (WIFSIGNALED(wstatus))
+		msh->g_msh->exit = WTERMSIG(wstatus) + 128;
+	msh->g_msh->child = 0;
 	return (EXIT_SUCCESS);
 }
 
+static void	launch(t_cmd *cmd, t_msh *msh)
+{
+}
+
+		execve(path, msh->cmds->cmd->argv.array, msh->env.envp);
 static void	child_process(int *tube, char **envp, int fd, t_info *state)
 {
 	if (dup2(fd, STDIN_FILENO) == -1 || close(fd) == -1)
@@ -91,4 +102,22 @@ int	exec_command(char **envp, int fd, t_info *state)
 		exec_command(envp, tube[0], state);
 	}
 	parent_end(state, pid, tube);
+}
+
+static t_builtinf	get_builtin(char const *cmd)
+{
+	t_builtinf const	builtins[N_BUILTIN] = {
+		cd, echo, env, exit, pwd, unset};
+	char const *const	names[N_BUILTIN] = {
+		"cd", "echo", "env", "exit", "pwd", "unset"};
+	size_t				i;
+
+	i = 0;
+	while (i < N_BUILTIN)
+	{
+		if (ft_strncmp(cmd, names[i], -1) == 0)
+			return (builtins[i]);
+		i++;
+	}
+	return (NULL);
 }
