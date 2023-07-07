77d91ded208e017f49a552d531dfff461ff70a30
Jeroen Van Halderen <jvan-hal@f0r3s9.codam.nl>
2023-07-07 11:06:13 +0200

    defined an undefined symbol

--- source/builtins/export.c
+++ source/builtins/export.c
@@ -6,11 +6,12 @@
 /*   By: jvan-hal <jvan-hal@student.codam.nl>         +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2023/04/20 16:51:03 by jvan-hal      #+#    #+#                 */
-/*   Updated: 2023/07/07 11:00:52 by jvan-hal      ########   odam.nl         */
+/*   Updated: 2023/07/07 11:05:25 by jvan-hal      ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ft_stdio.h"
+#include "ft_string.h"
 #include "msh.h"
 #include <fcntl.h>
 #include <unistd.h>
@@ -53,7 +54,7 @@ int	msh_export(t_cmd *cmd, t_msh *msh)
 	while (cmd->argv.array[i])
 	{
 		if (ft_strchr(cmd->argv.array[i], '='))
-            env_set(&msh->env, (const char)cmd->argv.array[i]);
+            env_set(&msh->env, (char const *)cmd->argv.array[i]);
 		++i;
 	}
 	return (0);
