73dac6df5c6a99fb5cd2004ba7ae7e0504a33c39
Jeroen Van Halderen <jvan-hal@f1r1s1.codam.nl>
2023-07-10 16:38:39 +0200

    wrong function prototype

--- include/msh_env.h
+++ include/msh_env.h
@@ -33,7 +33,7 @@ t_errno	env_init(t_env *env, char **envp);
 void	env_free(t_env *env);
 t_errno	env_set(t_env *env, char const *entry);
 t_errno env_append_val(t_env *env, char *entry, char const *name);
-void	env_unset(t_env *env, char const *name);
+t_errno env_unset(t_env *env, char const *name);
 char	*env_search(t_env *env, char const *name);
 
 // Utility functions.
