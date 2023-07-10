60dc5a02ff7ac29d1c5f26e97c7c4f76a2207295
Jeroen Van Halderen <jvan-hal@f1r1s1.codam.nl>
2023-07-10 17:13:39 +0200

    put env on the heap

--- test/unset_test/unset_unittest.c
+++ test/unset_test/unset_unittest.c
@@ -26,15 +26,46 @@ void redirect_stderr(void)
     cr_redirect_stderr();
 }
 
+void	env_free_(t_env *env)
+{
+    while (env->used--)
+        free(env->envp[env->used]);
+    free(env->envp);
+}
+
+t_errno	env_init_(t_env *env, int len, ...)
+{
+    va_list ap;
+    char *s;
+
+    env->envp = (char **)malloc(sizeof(char *) * (len + 1));
+    if (env->envp == NULL)
+        return (MSH_MEMFAIL);
+    env->len = len;
+    env->used = 0;
+    va_start(ap, len);
+    while (len > 0)
+    {
+        s = va_arg(ap, char *);
+        env->envp[env->used] = ft_strdup(s);
+        if (env->envp[env->used] == NULL)
+            return (env_free_(env), MSH_MEMFAIL);
+        env->used++;
+        len--;
+    }
+    va_end(ap);
+    env->envp[env->used] = NULL;
+    return (MSH_SUCCESS);
+}
+
 void	assert_unset_env(t_cmd *cmd, char *expected)
 {
 	t_msh	msh;
-	char	*environ[] = {"HOME=/tmp/cd-no_arg_with_home", "SHLVL=2", "LANG=en_US.UTF-8", NULL};
 
     cmd->io[1] = 1;
 	bzero(&msh, sizeof(msh));
-	msh.env.envp = environ;
-	msh.env.len = 3;
+    env_init_(&msh->env, 3, "HOME=/tmp/cd-no_arg_with_home", "SHLVL=2",
+              "LANG=en_US.UTF-8");
 	msh_unset(cmd, &msh);
 	print_2d_arr(cmd->io[1], msh.env.envp, msh.env.len);
 	fflush(stdout);
@@ -44,12 +75,11 @@ void	assert_unset_env(t_cmd *cmd, char *expected)
 void	assert_unset_output_error(t_cmd *cmd, char *expected)
 {
 	t_msh	msh;
-	char	*environ[] = {"HOME=/tmp/cd-no_arg_with_home", "SHLVL=2", "LANG=en_US.UTF-8", NULL};
 
     cmd->io[2] = 2;
 	bzero(&msh, sizeof(msh));
-	msh.env.envp = environ;
-	msh.env.len = 3;
+    env_init_(&msh->env, 3, "HOME=/tmp/cd-no_arg_with_home", "SHLVL=2",
+              "LANG=en_US.UTF-8");
 	msh_unset(cmd, &msh);
 	fflush(stderr);
 	cr_assert_stderr_eq_str(expected);
@@ -58,11 +88,10 @@ void	assert_unset_output_error(t_cmd *cmd, char *expected)
 void	assert_unset_envused(t_cmd *cmd, size_t expected)
 {
 	t_msh	msh;
-	char	*environ[] = {"HOME=/tmp/cd-no_arg_with_home", "SHLVL=2", "LANG=en_US.UTF-8", NULL};
 
 	bzero(&msh, sizeof(msh));
-	msh.env.envp = environ;
-	msh.env.len = 3;
+    env_init_(&msh->env, 3, "HOME=/tmp/cd-no_arg_with_home", "SHLVL=2",
+              "LANG=en_US.UTF-8");
 	msh_unset(cmd, &msh);
 	cr_assert_eq(msh.env.used, expected);
 }
