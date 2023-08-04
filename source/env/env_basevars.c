37e5f9baae1253f8f17ede2456e66ae18ee5deb0
David Bastings <dbasting@f1r5s6.codam.nl>
2023-08-04 13:50:15 +0200

    SHLVL will now be set to 1 if it hadn't been set before. SHELL will remain unchanged :'(

--- source/env/env_basevars.c
+++ source/env/env_basevars.c
@@ -6,7 +6,7 @@
 /*   By: dbasting <dbasting@codam.nl>                 +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2023/08/01 19:53:34 by dbasting      #+#    #+#                 */
-/*   Updated: 2023/08/01 23:56:14 by dbasting      ########   odam.nl         */
+/*   Updated: 2023/08/04 13:43:23 by dbasting      ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,8 +21,6 @@ static t_errno	set_shlvl(t_env *env);
 
 t_errno	env_basevars_set(t_env *env)
 {
-	if (env_update(env, "SHELL", "minishell") == 2)
-		return (msh_perror(0), MSH_MEMFAIL);
 	return (set_shlvl(env));
 }
 
@@ -33,7 +31,7 @@ static t_errno	set_shlvl(t_env *env)
 
 	shlvl_str = env_search(env, "SHLVL");
 	if (!shlvl_str)
-		return (env_set_entry(env, "SHLVL=0"));
+		return (env_set_entry(env, "SHLVL=1"));
 	shlvl = ft_atoi(shlvl_str) + 1;
 	if (shlvl >= 1000)
 	{
