e3e112ed5d306882c2f9694f947fc05d8512f54a
Jeroen Van Halderen <jvan-hal@f0r4s3.codam.nl>
2023-08-07 11:29:42 +0200

    fixed an error

--- source/var/var_parse.c
+++ source/var/var_parse.c
@@ -6,7 +6,7 @@
 /*   By: dbasting <dbasting@codam.nl>                 +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2023/05/22 08:45:12 by dbasting      #+#    #+#                 */
-/*   Updated: 2023/08/01 15:07:49 by dbasting      ########   odam.nl         */
+/*   Updated: 2023/08/07 11:27:59 by jvan-hal      ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -45,9 +45,9 @@ static inline size_t	is_varstr(char const *str)
 	size_t	i;
 
 	i = 0;
-	if (str[i] == '=' || str[i] == '+')
+	if (str[i] && (str[i] == '=' || str[i] == '+'))
 		return (0);
-	while (str[i] != '=')
+	while (str[i] && str[i] != '=')
 	{
 		if (str[i] == '\0')
 			return (0);
