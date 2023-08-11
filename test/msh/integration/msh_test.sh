a89d641f366cf673d08b92215f12d72948e47c5f
Jeroen Van Halderen <jvan-hal@f0r1s11.codam.nl>
2023-08-11 10:30:10 +0200

    added some nice tests for minishell

--- test/msh/integration/msh_test.sh
+++ test/msh/integration/msh_test.sh
@@ -6,21 +6,22 @@ STAT=0;
 test_list=(
 	"bonus_tests.txt"
 	"builtin_tests.txt"
-	"extras_tests.txt"
-	"pipes_tests.txt"
-	"redirects_tests.txt"
-	"syntax_tests.txt"
-	"wildcards_tests.txt"
+	# "extras_tests.txt"
+	# "pipes_tests.txt"
+	# "redirects_tests.txt"
+	# "syntax_tests.txt"
+	# "wildcards_tests.txt"
 	)
+chmod 000 ./test_files/invalid_permission
+mkdir ./outfiles
 for testfile in ${test_list[*]}; do
 	echo testing $testfile
 
 	while read teste; do
-		# echo "$teste"
-	OUT=$(echo -e "$teste" | ../../../minishell | grep -v '^msh$ ' | grep -v '^exit')
-	# echo "$OUT"
+	OUT=$(echo -e "$teste" | ../../../minishell | grep -v '^msh$ ' | grep -v '^exit$')
+	echo "$OUT"
 	BASHOUT=$(echo -e "$teste" | bash)
-	# echo "$BASHOUT"
+	echo "$BASHOUT"
 	if [ "$OUT" == "$BASHOUT" ]; then
 		echo 'OK'
 	else
@@ -31,5 +32,7 @@ for testfile in ${test_list[*]}; do
 done
 
 make fclean -C ../../..
+chmod 644 ./test_files/invalid_permission
+rm -rf ./outfiles
 
 exit $STAT
