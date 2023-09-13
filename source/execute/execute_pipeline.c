6f234766951eefb052c6345c4e9d7ace423c5a6e
Jeroen Van Halderen <jvan-hal@f1r5s12.codam.nl>
2023-09-13 17:03:11 +0200

    it works, a bit

--- source/execute/execute_pipeline.c
+++ source/execute/execute_pipeline.c
@@ -57,23 +57,24 @@ static t_errno	execute_pipeline_subsh(t_list **pipeline, t_msh *msh)
 	{
 		cmd = list_pop_ptr(pipeline);
 		pipe(tube);
+		if (*pipeline)
+			cmd->io[IO_OUT] = tube[PIPE_WRITE];
+		cmd->io[IO_IN] = read;
 		msh->child = fork();
 		if (msh->child == -1)
 			return (msh_perror(0), MSH_FORKFAIL);
 		if (msh->child == 0)
 		{
-			cmd->io[IO_IN] = read;
-			cmd->io[IO_OUT] = tube[PIPE_WRITE];
 			close(tube[PIPE_READ]);
 			execute_subsh(cmd, msh);
-		}
-		if (read > 0)
-		{
-			close(read);
 		}
 		read = tube[PIPE_READ];
 		close(tube[PIPE_WRITE]);
 		cmd_free(cmd);
+	}
+	if (read > 0)
+	{
+		close(read);
 	}
 	close(tube[PIPE_READ]);
 	msh->exit = execute_wait(msh);
