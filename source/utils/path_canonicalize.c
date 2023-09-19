bc603b50ba59b9daa26d979b54b667bc0f39dcf7
Jeroen Van Halderen <jvan-hal@f0r4s9.codam.nl>
2023-09-19 09:15:54 +0200

    finished working on the canonicalise function

--- source/utils/path_canonicalize.c
+++ source/utils/path_canonicalize.c
@@ -15,50 +15,47 @@
 #include "ft_string.h"
 #include <stddef.h>
 
-static void	removecurdir(char *str, size_t i);
-static void	removeprevdir(char *str, size_t i, size_t j);
-static void	removelastslash(char *str);
-static void	removeduplicateslash(char *str);
+static void		removecurdir(char *str, size_t i);
+static void		removeprevdir(char *str, size_t i);
+static size_t	lookforprevdir(char *str, size_t i);
+static void		removelastslash(char *str);
+static void		removeduplicateslash(char *str);
 
 void	path_canonicalize(char *str)
 {
 	size_t	i;
 
 	removeduplicateslash(str);
-	removelastslash(str);
+	if (str[1] != '\0')
+		removelastslash(str);
 	i = 0;
 	while (str[i])
 	{
 		if (str[i] == '.')
 		{
-			if (str[i + 1] && str[i + 2] && str[i + 1] == '.' && str[i + 2] == '/')
-			{
-				removeprevdir(str, i - 2, i + 3);
-				i += 3;
-			}
+			if (str[i + 1] && str[i + 1] == '.')
+				i += removeprevdir(str, i - 2);
 			else
-			{
 				i -= removecurdir(str, i);
-			}
 		}
 		++i;
 	}
-	removelastslash(str);
+	if (str[1] != '\0')
+		removelastslash(str);
 }
 
-static void	removeprevdir(char *str, size_t i, size_t j)
+static size_t	removeprevdir(char *str, size_t i)
 {
+	size_t	j;
+	size_t	dotlen;
+
+	dotlen = 2;
 	if (i < 0)
-		return ;
-	while (i >= 0)
-	{
-		if (str[i] == '/')
-			break ;
-		--i;
-		if (i == 0)
-			return ;
-	}
-	++i;
+		return (0);
+	if (str[i + 5] && str[i + 5] == '/')
+		dotlen = 3;
+	j = i + 2 + dotlen;
+	i = lookforprevdir(str, i);
 	while (str[j])
 	{
 		str[i] = str[j];
@@ -66,11 +63,29 @@ static void	removeprevdir(char *str, size_t i, size_t j)
 		++j;
 	}
 	str[i] = '\0';
+	return (dotlen);
+}
+
+static size_t	lookforprevdir(char *str, size_t i)
+{
+	size_t	j;
+
+	j = i;
+	while (i > 0)
+	{
+		if (i == 1)
+			return (j + 2);
+		if (str[i] == '/')
+			break ;
+		--i;
+	}
+	++i;
+	return (i);
 }
 
 static size_t	removecurdir(char *str, size_t i)
 {
-	size_t  j;
+	size_t	j;
 
 	j = i + 1;
 	while (str[j])
@@ -89,40 +104,40 @@ static size_t	removecurdir(char *str, size_t i)
 	return (j - i);
 }
 
-static void removelastslash(char *str)
+static void	removelastslash(char *str)
 {
-    size_t  i;
+	size_t	i;
 
-    i = ft_strlen(str);
-    --i;
-    if (str[i] == '/')
-        str[i] = '\0';
+	i = ft_strlen(str);
+	--i;
+	if (str[i] == '/')
+		str[i] = '\0';
 }
 
 static void	removeduplicateslash(char *str)
 {
-    size_t	i;
-    size_t	j;
-    size_t	k;
+	size_t	i;
+	size_t	j;
+	size_t	k;
 
-    i = 0;
-    while (str[i])
-    {
-        if (str[i] == '/')
-        {
-            k = i;
-            j = i + 1;
-            while (str[j] && str[j] == '/')
-                ++j;
-            ++k;
-            while (str[j])
-            {
-                str[k] = str[j];
-                ++k;
-                ++j;
-            }
-            str[k] = '\0';
-        }
-        ++i;
-    }
+	i = 0;
+	while (str[i])
+	{
+		if (str[i] == '/')
+		{
+			k = i;
+			j = i + 1;
+			while (str[j] && str[j] == '/')
+				++j;
+			++k;
+			while (str[j])
+			{
+				str[k] = str[j];
+				++k;
+				++j;
+			}
+			str[k] = '\0';
+		}
+		++i;
+	}
 }
